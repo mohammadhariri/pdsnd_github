fc62e1daf0cb7be127fa5beee97e05a43b035bed
mohammadhariri <mohammad.hariri@verizon.com>
2021-05-14 14:12:42 -0400

    Added first change to bikeshare.py for separator

--- bikeshare.py
+++ bikeshare.py
@@ -16,6 +16,7 @@ def get_filters():
         (str) month - name of the month to filter by, or "all" to apply no month filter
         (str) day - name of the day of week to filter by, or "all" to apply no day filter
     """
+    print('-'*40)
     print('Hello! Let\'s explore some US bikeshare data!')
     city=''
     month=0
